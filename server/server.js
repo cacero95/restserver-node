0df54f4195a0a52d9b2f12936336caa042f09c95
cacero95 <cacero95@hotmail.com>
2019-03-15 22:25:33 -0500

    creacion de las cabeceras Access control all origins

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const path = require('path');
  * para poder hallar cualquier contenido en nuestro proyecto
  */
 /**
- * cuando en la aplicacion se vea un .use() significa que estamos 
+ * cuando en la aplicacion se vea un .use() significa que estamos
  * llamando un middleware que funciona para ejecutarse siempre que
  * pase por esa parte el proyecto
  */
@@ -25,6 +25,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // con estos metodos preparo el proyecto a recivir JSON
 // parse application/json
 app.use(bodyParser.json());
+/*
+corps policy
+*/
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 /**
@@ -32,7 +43,7 @@ app.use(bodyParser.json());
  * client-id
  * 660910679754-7tgt311r4fd02nmubaj439bmo8ou36jl.apps.googleusercontent.com
  * client-secret
- * FgckiOl6zf8sjKYnkEAGyWya 
+ * FgckiOl6zf8sjKYnkEAGyWya
  */
 /**
  * habiltar la carpeta public para meter el html
